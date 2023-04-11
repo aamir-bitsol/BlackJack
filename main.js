0b7795a8ad208518faa799aa765e1e59d7467e70
Muhammad Aamir Zaman <aamir.zaman@bitsol.tech>
2023-04-11 15:35:57 +0500

    Typescript complied file added

---
+++ main.js
@@ -0,0 +1,70 @@
+// declaring variables with types
+var cards = [];
+var sum = 0;
+var hasBlackJack = false;
+var isAlive = false;
+var message = "";
+var user = {
+    name: "Muhammad Aamir Zaman",
+    prize: 200,
+};
+// accessing DOM elements
+var messageEl = document.getElementById("message-el");
+var sumEl = document.getElementById("sum-el");
+var cardsEl = document.getElementById("cards-el");
+var userEl = document.getElementById('user-info');
+if (userEl !== null) {
+    userEl.textContent = "".concat(user.name, " : $").concat(user.prize);
+}
+// generates a random number (1 to 13) and floor its output.
+function getRandomCard() {
+    var randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber > 10) {
+        return 10;
+    }
+    else if (randomNumber === 1) {
+        return 11;
+    }
+    else {
+        return randomNumber;
+    }
+}
+function startGame() {
+    isAlive = true;
+    cards = [getRandomCard(), getRandomCard()];
+    sum = cards[0] + cards[1];
+    renderGame();
+}
+// displays cards and checks if user got blackjack or user wants to draw a card or he is out of the game
+function renderGame() {
+    if (cardsEl !== null && sumEl !== null) {
+        cardsEl.textContent = "Cards: ";
+        for (var i = 0; i < cards.length; i++) {
+            cardsEl.textContent += cards[i] + " ";
+        }
+        sumEl.textContent = "Sum: " + sum;
+        if (sum <= 20) {
+            message = "Do you want to draw a new card?";
+        }
+        else if (sum === 21) {
+            message = "You've got Blackjack!";
+            hasBlackJack = true;
+        }
+        else {
+            message = "You're out of the game!";
+            isAlive = false;
+        }
+        if (messageEl !== null) {
+            messageEl.textContent = message;
+        }
+    }
+}
+// checks if user is eligible for card draw and generates a random card for user
+function newCard() {
+    if (isAlive && !hasBlackJack) {
+        var card = getRandomCard();
+        sum += card;
+        cards.push(card);
+        renderGame();
+    }
+}
